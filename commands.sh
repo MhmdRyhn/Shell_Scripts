40b5e21c75f9dd5266bac81f9341491498abbec3
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2018-11-03 09:55:24 +0600

    shortcut for c command

--- commands.sh
+++ commands.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+
+# Shortcut to run c program.
+ccode()
+{
+	: '
+	command to run C program.
+
+	How to use: 
+	Enter the follwing command in terminal to run the C file.
+
+	c filename.c
+	
+	'
+	gcc $1 -o obj
+	./obj
+}
+
+
 # Shortcut to run c++ program.
 cpp()
 {
@@ -10,6 +28,7 @@ cpp()
 	Enter the follwing command in terminal to run the c++ file.
 
 	cpp filename.cpp
+
 	'
 	g++ $1
 	./a.out
